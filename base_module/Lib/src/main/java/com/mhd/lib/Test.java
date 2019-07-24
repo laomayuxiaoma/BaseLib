e5a59db3dc0359578e5b908a4b1cc7b6219f8150
zhangming <a>
2019-07-24 14:30:09 +0800

    Initial commit

---
+++ base_module/Lib/src/main/java/com/mhd/lib/Test.java
@@ -0,0 +1,14 @@
+package com.mhd.lib;
+
+/**
+ * @author zhangming
+ * @Date 2019/7/24 14:28
+ * @Description: 提交测试
+ */
+public class Test {
+
+    public static String getStr() {
+        return "测试";
+    }
+
+}
