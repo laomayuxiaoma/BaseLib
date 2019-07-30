25ceed65a1142bcbe3a21b4693e3cf774118a168
wangfei <f.wang@muheda.com>
2019-07-30 11:26:49 +0800

    移除无用代码

--- app/src/main/java/com/mhd/baselib/view/activity/MainActivity.java
+++ app/src/main/java/com/mhd/baselib/view/activity/MainActivity.java
@@ -44,8 +44,7 @@ public class MainActivity extends AppCompatActivity {
         initView();
         initData();
         initAdapter();
-
-        SearchView
+        
         //测试
 //        rvList.postDelayed(new Runnable() {
 //            @Override
